e3fb3b647563f83c4b715ae893b20975e57099ff
Tiva Rocco <143124479+tiva710@users.noreply.github.com>
2023-10-24 21:19:36 -0700

    Create User.java

---
+++ cst438_gradebook/src/main/java/com/cst438/domain/User.java
@@ -0,0 +1,67 @@
+package com.cst438.domain;
+import java.security.Principal;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
+@Entity
+@Table(name="user_table") //CREATE TABLE 
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id; 
+	private String alias;
+	private String email;
+	private String firstName; 
+	private String lastName;
+	private String password; 
+	private String role; 
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAlias() {
+		return alias;
+	}
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	} 
+}
