2202523e21ab4d815b84649ebefe1170cc82d991
Tiva Rocco <143124479+tiva710@users.noreply.github.com>
2023-10-29 12:06:56 -0700

    Update UserRepository.java

--- cst438_gradebook/src/main/java/com/cst438/domain/UserRepository.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/UserRepository.java
@@ -1,7 +1,10 @@
 package com.cst438.domain;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
 
 
-public interface UserRepository extends CrudRepository <Course, Integer> {
-
+public interface UserRepository extends CrudRepository <User, Integer> {
+  @Query("select a from User a where a.email order by a.id")
+	User findByEmail(@Param("email") String email);
 }
